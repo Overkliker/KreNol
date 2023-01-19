fd691a6adacc444c7652c74fe08ecce5d733db4b
Данила Хренов <danilahreno@gmail.com>
2023-01-19 21:00:58 +0300

    хуй

--- KreNol/MainWindow.xaml.cs
+++ KreNol/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace KreNol
     {
         public bool isGoing = true;
         public int thisGame = 0;
-        List<(int, int, int)> allWins = new List<(int, int, int)> { (0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6) };
+        List<(int, int, int)> allWins = new List<(int, int, int)> { (0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (2, 5, 8), (0, 4, 8), (2, 4, 6) };
         public MainWindow()
         {
             InitializeComponent();
